4645ce2692215a7614343c43d257bdefe276734b
CihanKurtDev <cihankurt.job@gmail.com>
2023-10-31 15:14:12 +0100

    close button for form only when creating Folder

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,19 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import userStore from '../stores/userStore';
 
 export default function Form({ className = "form light", fields, submitFunction, extra = [] }) {
   const navigate = useNavigate()
+  const location = useLocation().pathname.slice(1)
   const {setShowFolderForm} = userStore()
 
   return (
     <div className='form-wrapper'>
       <form className={className} onSubmit={(e) => submitFunction(e, navigate)}>
+        {location === "Home" &&
         <div className='button__wrapper--flex-end'>
           <button className="button button--close" onClick={() => setShowFolderForm(false)}>x</button>
         </div>
+        }
         <div className="formInfoWrapper">
           {fields.map((field, index) => (
             <label key={index} className="form__label" htmlFor={field.id}>
